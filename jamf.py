873040ecaa827952563543b6f9522a764488a28b
thedzy <thedzy@hotmail.com>
2020-01-19 21:21:31 +0100

    Minor improvements Removed references to "response" and kept data Added disable ssl waraning Added set and retrieve url

--- jamf.py
+++ jamf.py
@@ -16,6 +16,7 @@ __email__ = 'thedzy@hotmail.com'
 __status__ = 'Development'
 
 import requests
+import urllib3
 
 
 class APIResponse:
@@ -23,28 +24,26 @@ class APIResponse:
     Data object containing data of the query
     :property success: (bool) success of the call
     :property url: (str) url that was called
-    :property response: (str, json) depending on the success
+    :property data: (str, json) depending on the success
     :property http_code: (int) http code returned
     :property err: (str) Error if exception
     """
 
-    def __init__(self, success=False, url=None, response=None, http_code=0, err=None, **kwargs):
+    def __init__(self, success=False, url=None, data=None, http_code=0, err=None, **kwargs):
         """
         Initialisation method
         :param success: (bool) success of the call
         :param url: (str) url that was called
-        :param response: (str, json) depending on the success
+        :param data: (str, json) depending on the success
         :param http_code: (int) http code returned
         :param err: (str) Error if exception
         :param kwargs: (dict)
         """
         self.success = kwargs['success'] if 'success' in kwargs else success
         self.url = kwargs['url'] if 'url' in kwargs else url
-        self.response = kwargs['response'] if 'response' in kwargs else response
+        self.data = kwargs['data'] if 'data' in kwargs else data
         self.http_code = kwargs['http_code'] if 'http_code' in kwargs else http_code
         self.err = kwargs['err'] if 'err' in kwargs else err
-
-        self.data = self.response
 
     def success(self, success=None):
         """
@@ -56,15 +55,15 @@ class APIResponse:
 
         return self.success
 
-    def response(self, response=None):
+    def data(self, new_data=None):
         """
-        :param response: Set or retrieve property response
+        :param new_data: Set or retrieve property data
         :return: (int) Current/new setting
         """
-        if response is not None:
-            self.response = self.data = response
+        if new_data is not None:
+            self.data = new_data
 
-        return self.response
+        return self.data
 
     def http_code(self, http_code=None):
         """
@@ -92,17 +91,17 @@ class JamfClassic:
     JamfClassic interacts with the classic API of Jamf
     """
 
-    def __init__(self, api_url, usernmae, password, *args, **kwargs):
+    def __init__(self, api_url, username, password, **kwargs):
         """
         Initialisation method
         :param api_url: (str) url of the api
-        :param usernmae: (str) username
+        :param username: (str) username
         :param password: (str) password
         :param args: (list)
         :param kwargs: (dict)
         """
         self._api_url = api_url
-        self._usernmae = usernmae
+        self._username = username
         self._password = password
 
         self._headers = {
@@ -112,6 +111,10 @@ class JamfClassic:
         }
         self._timeout = int(kwargs['timoout']) if 'timeout' in kwargs else 240.0
         self._verify = bool(kwargs['verify']) if 'verify' in kwargs else True
+        self._disable_warnings = bool(kwargs['disable_warnings']) if 'disable_warnings' in kwargs else False
+
+        if self._disable_warnings:
+            urllib3.disable_warnings()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         """
@@ -121,7 +124,7 @@ class JamfClassic:
         :param exc_tb: None
         :return: (void)
         """
-        del self._usernmae
+        del self._username
         del self._password
 
     def timeout(self, timeout=None):
@@ -144,7 +147,28 @@ class JamfClassic:
         if verify is not None and isinstance(verify, bool):
             self._verify = verify
 
-    def get_data(self, *objects, **kwargs):
+    def url(self, url=None):
+        """
+        Set or retrieve the url
+        :param url: (str) new value or (None) to remain
+        :return: (str) Current/new setting
+        """
+        if url is not None:
+            self._api_url = url
+
+        return self._api_url
+
+    @staticmethod
+    def disable_warnings():
+        """
+        Disable warnings for ssl verify
+        Making unverified HTTPS requests is strongly discouraged, however,
+        if you understand the risks and wish to disable these warnings, you can use disable_warnings()
+        :return: (void))
+        """
+        urllib3.disable_warnings()
+
+    def get_data(self, *objects):
         """
         GET from the api
         :param objects: (list) of objects ex. /JSSResource/computer/id/0 = [ 'computer', 'id', 0]
@@ -152,12 +176,12 @@ class JamfClassic:
         :return:
         """
         if not objects:
-            return APIResponse(response='No object specified')
+            return APIResponse(data='No object specified')
 
         # Get data
         request_url = '{0}/JSSResource/{1}'.format(self._api_url, '/'.join(str(arg) for arg in objects))
         try:
-            request = requests.get(request_url, auth=(self._usernmae, self._password),
+            request = requests.get(request_url, auth=(self._username, self._password),
                                    headers=self._headers, timeout=self._timeout, verify=self._verify)
         except requests.exceptions.HTTPError as err:
             return APIResponse(url=request_url, err=err)
@@ -167,7 +191,7 @@ class JamfClassic:
         else:
             return APIResponse(False, request_url, request.text, request.status_code)
 
-    def del_data(self, *objects, **kwargs):
+    def del_data(self, *objects):
         """
         DELETE to the api
         :param objects: (list) of objects ex. /JSSResource/computer/id/0 = [ 'computer', 'id', 0]
@@ -175,12 +199,12 @@ class JamfClassic:
         :return:
         """
         if not objects:
-            return APIResponse(response='No object specified')
+            return APIResponse(data='No object specified')
 
-        # Dlete data
+        # Delete data
         request_url = '{0}/JSSResource/{1}'.format(self._api_url, '/'.join(str(arg) for arg in objects))
         try:
-            request = requests.delete(request_url, auth=(self._usernmae, self._password),
+            request = requests.delete(request_url, auth=(self._username, self._password),
                                       headers=self._headers, timeout=self._timeout, verify=self._verify)
         except requests.exceptions.HTTPError as err:
             return APIResponse(url=request_url, err=err)
@@ -190,7 +214,7 @@ class JamfClassic:
         else:
             return APIResponse(False, request_url, request.text, request.status_code)
 
-    def put_data(self, data, *objects, **kwargs):
+    def put_data(self, data, *objects):
         """
         PUT to the api
         :param data: (dict) data to post
@@ -199,12 +223,12 @@ class JamfClassic:
         :return:
         """
         if not objects:
-            return APIResponse(response='No object specified')
+            return APIResponse(data='No object specified')
 
         # Put data
         request_url = '{0}/JSSResource/{1}'.format(self._api_url, '/'.join(str(arg) for arg in objects))
         try:
-            request = requests.put(request_url, auth=(self._usernmae, self._password),
+            request = requests.put(request_url, auth=(self._username, self._password),
                                    headers=self._headers, timeout=self._timeout, verify=self._verify, data=data)
         except requests.exceptions.HTTPError as err:
             return APIResponse(url=request_url, err=err)
@@ -214,7 +238,7 @@ class JamfClassic:
         else:
             return APIResponse(False, request_url, request.text, request.status_code)
 
-    def post_data(self, data, *objects, **kwargs):
+    def post_data(self, data, *objects):
         """
         POST to the api
         :param data: (dict) data to post
@@ -223,12 +247,12 @@ class JamfClassic:
         :return:
         """
         if not objects:
-            return APIResponse(response='No object specified')
+            return APIResponse(data='No object specified')
 
         # Post data
         request_url = '{0}/JSSResource/{1}'.format(self._api_url, '/'.join(str(arg) for arg in objects))
         try:
-            request = requests.post(request_url, auth=(self._usernmae, self._password),
+            request = requests.post(request_url, auth=(self._username, self._password),
                                     headers=self._headers, timeout=self._timeout, verify=self._verify, data=data)
         except requests.exceptions.HTTPError as err:
             return APIResponse(url=request_url, err=err)
@@ -241,20 +265,20 @@ class JamfClassic:
 
 class JamfUAPI:
     """
-    JAMFUAP interacts with the universal API of Jamf
+    JamfUAPI interacts with the universal API of Jamf
     """
 
-    def __init__(self, api_url, username, password, *args, **kwargs):
+    def __init__(self, api_url, username, password, **kwargs):
         """
         Initialisation method
         :param api_url: (str) url of the api
-        :param usernmae: (str) username
+        :param username: (str) username
         :param password: (str) password
         :param args: (list)
         :param kwargs: (dict)
         """
         self._api_url = api_url
-        self._usernmae = username
+        self._username = username
         self._password = password
         self._token = None
 
@@ -265,6 +289,10 @@ class JamfUAPI:
         }
         self._timeout = int(kwargs['timoout']) if 'timeout' in kwargs else 240.0
         self._verify = bool(kwargs['verify']) if 'verify' in kwargs else True
+        self._disable_warnings = bool(kwargs['disable_warnings']) if 'disable_warnings' in kwargs else False
+
+        if self._disable_warnings:
+            urllib3.disable_warnings()
 
         self._login()
 
@@ -293,7 +321,7 @@ class JamfUAPI:
         request_url = self._api_url + '/uapi/auth/tokens'
 
         try:
-            request = requests.post(request_url, auth=(self._usernmae, self._password),
+            request = requests.post(request_url, auth=(self._username, self._password),
                                     headers=self._headers, timeout=self._timeout, verify=self._verify, data=None)
         except requests.exceptions.HTTPError as err:
             return APIResponse(url=request_url, err=err)
@@ -337,6 +365,17 @@ class JamfUAPI:
 
         return self._timeout
 
+    def url(self, url=None):
+        """
+        Set or retrieve the url
+        :param url: (str) new value or (None) to remain
+        :return: (str) Current/new setting
+        """
+        if url is not None:
+            self._api_url = url
+
+        return self._api_url
+
     def verify_ssl(self, verify=None):
         """
         Set or retrieve whether to verify teh SSL certificate
@@ -347,6 +386,16 @@ class JamfUAPI:
             self._verify = verify
 
         return self._verify
+
+    @staticmethod
+    def disable_warnings():
+        """
+        Disable warnings for ssl verify
+        Making unverified HTTPS requests is strongly discouraged, however,
+        if you understand the risks and wish to disable these warnings, you can use disable_warnings()
+        :return: (void))
+        """
+        urllib3.disable_warnings()
 
     def get_login(self):
         """
@@ -363,7 +412,7 @@ class JamfUAPI:
         :return: (APIResponse)
         """
         if not objects:
-            return APIResponse(response='No object specified')
+            return APIResponse(data='No object specified')
 
         options = []
         for kwarg in kwargs:
@@ -394,7 +443,7 @@ class JamfUAPI:
         :return: (APIResponse)
         """
         if not objects:
-            return APIResponse(response='No object specified')
+            return APIResponse(data='No object specified')
 
         options = []
         for kwarg in kwargs:
@@ -426,7 +475,7 @@ class JamfUAPI:
         :return: (APIResponse)
         """
         if not objects:
-            return APIResponse(response='No object specified')
+            return APIResponse(data='No object specified')
 
         options = []
         for kwarg in kwargs:
@@ -458,7 +507,7 @@ class JamfUAPI:
         :return: (APIResponse)
         """
         if not objects:
-            return APIResponse(response='No object specified')
+            return APIResponse(data='No object specified')
 
         options = []
         for kwarg in kwargs:
@@ -480,3 +529,4 @@ class JamfUAPI:
             return APIResponse(True, url=request_url, http_code=request.status_code)
         else:
             return APIResponse(False, url=request_url, http_code=request.status_code)
+
