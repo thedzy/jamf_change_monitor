a21a70bc502ab1d43b62465f329d488f97e4bfb1
thedzy <thedzy@hotmail.com>
2020-01-19 21:22:59 +0100

    Created timer decorator Simplied the timing of modules but using a decorator

--- modules_common.py
+++ modules_common.py
@@ -15,20 +15,35 @@ __maintainer__ = 'thedzy'
 __email__ = 'thedzy@hotmail.com'
 __status__ = 'Development'
 
+import logging
+import os
 import time
 
 
-def runtime_message(start_time, path):
-    """
-    Display total run time message
-    :param start_time: (int) Epoch startime
-    :param path: (str) module name
-    :return: (str) message
-    """
-    minutes, seconds = divmod(time.time() - start_time, 60)
-    hours, minutes = divmod(minutes, 60)
+def timer(script_file=None):
+    def timer_wrapper(func):
+        def timer_func(*args, **kwargs):
+            # Get the start time
+            start_time = time.time()
 
-    return 'Total runtime for {3}: {0:.0f}:{1:.0f}:{2:.3f}'.format(hours, minutes, seconds, path)
+            if script_file is None:
+                module = 'undefined'
+            else:
+                module = os.path.basename(script_file).split('.')[0]
 
+            logging.info('Starting {}'.format(module))
 
+            # Run function
+            result = func(*args, **kwargs)
+
+            # Print timeer
+            minutes, seconds = divmod(time.time() - start_time, 60)
+            hours, minutes = divmod(minutes, 60)
+            logging.info('Total runtime for {3}: {0:.0f}:{1:.0f}:{2:.3f}'.format(hours, minutes, seconds, module))
+
+            return result
+
+        return timer_func
+
+    return timer_wrapper
 
