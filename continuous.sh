f6354906fa51300a8ec57445a595fc97468e05e4
thedzy <thedzy@thedzy.com>
2021-12-10 22:18:41 -0500

    Corrected typos, marked as legacy

--- continuous.sh
+++ continuous.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+# Kept for legacy reasons, however a daemon would be a wiser approach
+
 # Run this for continual running of the code for faster alerts.
 # Note: This will increase the strain on your API
-#   Cconsider dedicating a node or increasing the performance of your nodes to accomidate is recommended
+#   Consider dedicating a node or increasing the performance of your nodes to accommodate is recommended
 
 # The time to wait between runs in minutes
 STAGGER_TIME=3
@@ -11,12 +13,12 @@ STAGGER_TIME=3
 BASE_DIR=$(dirname $0)
 
 if [ "$1" == "--help" ]; then
-    printf "Usage: continous.sh [options]\n  Run jamf_change_monitor.py every %d minutes\n"  $STAGGER_TIME
+    printf "Usage: continuous.sh [options]\n  Run jamf_change_monitor.py every %d minutes\n"  $STAGGER_TIME
    "$BASE_DIR/jamf_change_monitor.py" "${@}"
     exit 0
 fi
 
-# Run the python script in an idefinate loop passing paraments from scrip python
+# Run the python script in an indefinite loop passing parameters from scrip python
 while true; do
   "$BASE_DIR/bin/python3" "$BASE_DIR/jamf_change_monitor.py" "${@}"
   sleep $(( STAGGER_TIME * 60 ))
